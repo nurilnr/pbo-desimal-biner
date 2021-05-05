99bd71485056c35680556cae64c0a2c98421fc49
Mohammad Nuril Rizki <nurilnr22@gmail.com>
2021-05-05 22:25:21 +0700

    DeretanPrimaKonsepArray

---
+++ BilanganPrimaArray/BilanganPrimaArray/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BilanganPrimaArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.Write("Masukkan Batas Bilangan Prima : ");
+
+			bool prima = true;
+
+			int bilangan = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Bilangan Primanya adalah: ");
+
+			if (bilangan >= 2)
+			{
+				for (int i = 2; i <= bilangan; i++)
+				{
+					for (int j = 2; j < i; j++)
+					{
+						if ((i % j) == 0)
+						{
+							prima = false;
+							break;
+						}
+					}
+
+					if (prima)
+						Console.Write(i + " ");
+					prima = true;
+				}
+			}
+			else
+				Console.WriteLine("Tidak ada bilangan prima yang bisa dituliskan");
+			Console.ReadLine();
+		}
+
+
+	}
+    
+}
