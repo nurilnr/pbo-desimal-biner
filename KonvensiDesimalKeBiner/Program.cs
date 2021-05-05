f0104418afcbbfb22d5719b1df42ba5dabe267f1
Mohammad Nuril Rizki <nurilnr22@gmail.com>
2021-05-05 21:53:32 +0700

    first commit

---
+++ KonvensiDesimalKeBiner/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace KonvensiDesimalKeBiner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int desimal, sisa;
+            Console.WriteLine("Masukan Bilangan Desimal :");
+            desimal = Convert.ToInt32(Console.ReadLine());
+            string hasil = "";
+            while (desimal > 0)
+            {
+                sisa = desimal % 2;
+                desimal /= 2;
+                hasil = sisa.ToString() + hasil;
+            }
+            Console.WriteLine("Bilangan Biner : {0}", hasil);
+            Console.ReadLine();
+        }
+    }
+}
